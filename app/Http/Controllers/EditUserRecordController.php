f425286cdc431a57cbe4e6a7810fc8ddf4535736
Vinícius <vinicius.luiz.9256@gmail.com>
2023-01-20 18:07:17 -0300

    Criado o controller EdituserRecordController

---
+++ app/Http/Controllers/EditUserRecordController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+
+class EditUserRecordController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $users = User::find($id);
+        return view('editUserRecord', ['users'=>$users, 'id' => $id]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        Validator::make(
+            $request->all(),
+            [
+                'name' => 'required|string',
+                'email' => 'required|string',
+            ],
+            [
+                'required' => 'O campo :attribute é obrigatório.',
+            ]
+        )->validate();
+        
+        if ($request->password !== $request->Confirm_Password)
+            return back()->withErrors(["cadastro" => "Senha e Confirmar Senha não combinam!"]);
+            
+        $user = User::find($request->id);
+        $user->name = $request->name;
+        $user->occupation = $request->occupation;
+        $user->email = $request->email;
+        $user->cpf = $request->cpf;
+        $user->password = Hash::make($request->password);
+        $user->save();
+        return back()->with("sucess", "Usuario atualizado com sucesso");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
