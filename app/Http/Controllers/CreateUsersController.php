2c9020f8514f6844e86cd4058d740cf81d739b2f
Vinícius <vinicius.luiz.9256@gmail.com>
2023-01-24 14:11:13 -0300

    criado controller para cadastro de usuário

---
+++ app/Http/Controllers/CreateUsersController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\userRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class CreateUsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('createUsers');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(userRequest $request)
+    {
+        function FormatCpf($requestCpf){
+            // Extrai somente os números
+            $cpf = preg_replace( '/[^0-9]/is', '', $requestCpf );
+            return $cpf;
+        }
+
+        // salvar usuário.
+        $user = new User();
+        $user->name = $request->name;
+        $user->occupation = $request->occupation;
+        $user->email = $request->email;
+        $user->cpf = FormatCpf($request->cpf);
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+        return back()->with('sucess', 'Cadastro realizado com sucesso!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
