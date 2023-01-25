f6440f62dbb2033b2cc4e34bd7b34cb469a570b4
Vinícius <vinicius.luiz.9256@gmail.com>
2023-01-25 11:00:00 -0300

    modificação de regras para senha

--- app/Http/Requests/userRequest.php
+++ app/Http/Requests/userRequest.php
@@ -26,7 +26,7 @@ class userRequest extends FormRequest
     {
         return [
             'name'                  =>  ['required','string'],
-            'password'              =>  ['required','confirmed','min:8'],
+            'password'              =>  ['confirmed'],
             'email'                 =>  ['required','email', "unique:users,email,{$this->id}"],
             'occupation'            =>  ['required','string'],
             'cpf'                   =>  ["required","unique:users,cpf,{$this->id}",new RightCpf],
