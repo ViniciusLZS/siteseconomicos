917924ff5e7491c8e1ffa1df6b031003e89cf6a7
Vinícius <vinicius.luiz.9256@gmail.com>
2023-01-25 10:59:36 -0300

    Criado request para cadastro

---
+++ app/Http/Requests/signUpUser.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\RightCpf;
+use Illuminate\Foundation\Http\FormRequest;
+
+class signUpUser extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'                  =>  ['required','string'],
+            'password'              =>  ['required','confirmed','min:8'],
+            'email'                 =>  ['required','email', "unique:users,email,{$this->id}"],
+            'occupation'            =>  ['required','string'],
+            'cpf'                   =>  ["required","unique:users,cpf,{$this->id}",new RightCpf],
+        ];
+    }
+    public function attributes()
+    {
+        return [
+            'name' => 'nome',
+            'email' => 'email',
+            'occupation' => 'função',
+            'cpf' => 'CPF'
+        ];
+    }
+
+    // public function messages()
+    // {
+    //     //
+    // }
+}
