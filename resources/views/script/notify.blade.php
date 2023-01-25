f6b2d08158538fadbc485e82ebf14e1173e52f9f
Vinícius <vinicius.luiz.9256@gmail.com>
2023-01-25 13:51:18 -0300

    Alteração na notificação

--- resources/views/script/notify.blade.php
+++ resources/views/script/notify.blade.php
@@ -1,17 +1,56 @@
 	<!-- Notification -->
-@if (session('sucess'))
-	<script>
-		$.notify({
-			icon: 'flaticon-alarm-1',
-			title: 'Notificação',
-			message: "{{ session('sucess') }}",
-		},{
-			type: 'success',
-			placement: {
-				from: "bottom",
-				align: "right"
-			},
-			time: 1000,
-		});
-	</script>
-@endif
+
+@foreach (session()->all() as $msg)
+
+	@if ($msg == session('success'))
+		<script>
+		{{-- console.log("{{$msg}}"); --}}
+			$.notify({
+				icon: 'flaticon-alarm-1',
+				title: 'Notificação',
+				message: "{{ session('success') }}",
+			},{
+				type: 'success',
+				placement: {
+					from: "bottom",
+					align: "right"
+				},
+				time: 1000,
+			});
+		</script>
+	@endif
+
+	@if ($msg == session('warning'))
+		<script>
+			$.notify({
+				icon: 'flaticon-alarm-1',
+				title: 'Notificação',
+				message: "{{ session('warning') }}",
+			},{
+				type: 'warning',
+				placement: {
+					from: "bottom",
+					align: "right"
+				},
+				time: 1000,
+			});
+		</script>
+	@endif
+
+	@if ($msg == session('danger'))
+		<script>
+			$.notify({
+				icon: 'flaticon-alarm-1',
+				title: 'Notificação',
+				message: "{{ session('danger') }}",
+			},{
+				type: 'danger',
+				placement: {
+					from: "bottom",
+					align: "right"
+				},
+				time: 1000,
+			});
+		</script>
+	@endif
+@endforeach
